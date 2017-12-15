277f467a275ed405b190d8d22e2e099e19c4724f
Matthew Croak <matthewcroak@Matthews-MBP.home>
2017-12-15 17:00:19 -0500

    I think I had a breakthrough, it's ugly, but I think I figured out how to either find a company lead and then create the interview or to create a new company lead and then create the interview

--- app/controllers/company_lead_interviews_controller.rb
+++ app/controllers/company_lead_interviews_controller.rb
@@ -11,8 +11,20 @@ class CompanyLeadInterviewsController < ApplicationController
     end
 
     def create
-      byebug
-      @company_lead_interview = CompanyLeadInterview.new(company_lead_interview_params)
+
+      let @company_lead_found = CompanyLead.wherePermission.where(:first_name=>company_lead_interview_params[:first_name]).where(:last_name=>company_lead_interview_params[:last_name], email_address: company_lead_interview_params[:email_address]).first
+
+      if !company_lead_interview_params[:company_lead_id]
+        if !@company_lead_found
+          @company_lead = CompanyLead.create(first_name: company_lead_interview_params[:first_name], last_name: company_lead_interview_params[:last_name], email_address: company_lead_interview_params[:email_address], phone_number: company_lead_interview_params[:phone_number], licensed: company_lead_interview_params[:licensed])
+
+          @company_lead_interview = CompanyLeadInterview.new(title: company_lead_interview_params[:title], trainer_id: company_lead_interview_params[:trainer_id], date: company_lead_interview_params[:date], location: company_lead_interview_params[:location], company_lead_id: @company_lead.id)
+
+        elsif @company_lead_found
+          @company_lead_interview = CompanyLeadInterview.new(title: company_lead_interview_params[:title], trainer_id: company_lead_interview_params[:trainer_id], date: company_lead_interview_params[:date], location: company_lead_interview_params[:location], company_lead_id: @company_lead_found.id)
+        end
+      end
+
       if @company_lead_interview.save
         render json: {company_lead_interview: @company_lead_interview}
       else
@@ -22,7 +34,7 @@ class CompanyLeadInterviewsController < ApplicationController
 
     def update
       @company_lead_interview = CompanyLeadInterview.find(params[:id])
-      if @company_lead_interview.update(company_lead_interview_params)
+      if @company_lead_interview.update(title: company_lead_interview_params[:title], trainer_id: company_lead_interview_params[:trainer_id], date: company_lead_interview_params[:date], location: company_lead_interview_params[:location], company_lead_id: company_lead_interview_params[:company_lead_id])
         render json: @company_lead_interview
       else
         render json: {error: @company_lead_interview.errors.messages.first[1][0]}, status: 406
@@ -38,7 +50,7 @@ class CompanyLeadInterviewsController < ApplicationController
     private
 
     def company_lead_interview_params
-      params.permit(:title, :trainer_id, :date, :location, :company_lead_id, :notes)
+      params.permit(:title, :trainer_id, :date, :location, :company_lead_id, :notes, :email_address, :first_name, :last_name, :phone_number, :licensed)
     end
 
 end
