94e6ef1e14f9659f7dd4b4e864b77d357f37b423
Matthew Croak <matthewcroak@Matthews-MacBook-Pro.local>
2017-12-17 16:50:45 -0500

    Was able to get round robin functionality to work.

--- app/controllers/company_lead_interviews_controller.rb
+++ app/controllers/company_lead_interviews_controller.rb
@@ -11,7 +11,10 @@ class CompanyLeadInterviewsController < ApplicationController
     end
 
     def create
+      @free_trainers = Trainer.all.where(:occupied=>false, :hold=>false).sort_by(&:updated_at)
+      @first_free_trainer = @free_trainers.first
       if !company_lead_interview_params[:company_lead_id]
+        byebug
         @company_lead = CompanyLead.where(:first_name=>company_lead_interview_params[:first_name], :last_name=>company_lead_interview_params[:last_name], email_address: company_lead_interview_params[:email_address]).first_or_create do |company_lead|
           company_lead.first_name = company_lead_interview_params[:first_name]
           company_lead.last_name = company_lead_interview_params[:last_name]
@@ -19,8 +22,7 @@ class CompanyLeadInterviewsController < ApplicationController
           company_lead.phone_number = company_lead_interview_params[:phone_number]
           company_lead.licensed = company_lead_interview_params[:licensed]
         end
-
-        @company_lead_interview = CompanyLeadInterview.new(title: company_lead_interview_params[:title], trainer_id: company_lead_interview_params[:trainer_id], date: company_lead_interview_params[:date], location: company_lead_interview_params[:location], company_lead_id: @company_lead.id)
+        @company_lead_interview = CompanyLeadInterview.new(trainer_id: @first_free_trainer.id, date: company_lead_interview_params[:date], location: company_lead_interview_params[:location], company_lead_id: @company_lead.id)
       end
 
       if @company_lead_interview.save
@@ -31,11 +33,17 @@ class CompanyLeadInterviewsController < ApplicationController
     end
 
     def update
+      let @free_trainers = Trainer.all.where(occupied: false)
+      @trainer = Trainer.all.find(company_lead_interview_params[:trainer_id])
       @company_lead_interview = CompanyLeadInterview.find(params[:id])
-      if @company_lead_interview.update(title: company_lead_interview_params[:title], trainer_id: company_lead_interview_params[:trainer_id], date: company_lead_interview_params[:date], location: company_lead_interview_params[:location], company_lead_id: company_lead_interview_params[:company_lead_id])
-        render json: @company_lead_interview
+      if !@free_trainers.include(@trainer)
+        render json: {error: "trainer unavailable"}
       else
-        render json: {error: @company_lead_interview.errors.messages.first[1][0]}, status: 406
+        if @company_lead_interview.update(title: company_lead_interview_params[:title], trainer_id: company_lead_interview_params[:trainer_id], date: company_lead_interview_params[:date], location: company_lead_interview_params[:location], company_lead_id: company_lead_interview_params[:company_lead_id])
+          render json: @company_lead_interview
+        else
+          render json: {error: @company_lead_interview.errors.messages.first[1][0]}, status: 406
+        end
       end
     end
 
