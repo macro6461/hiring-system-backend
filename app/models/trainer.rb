ac5a66bcf2a8d7bf8593252d4528429c86c034af
Matthew Croak <matthewcroak@Matthews-MacBook-Pro.local>
2017-12-17 16:52:42 -0500

    need to add these individually, too many big changes I guess

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -4,4 +4,31 @@ class Trainer < ApplicationRecord
   has_many :company_lead_interviews
   has_many :trainer_leads
 
+  after_save :order_trainers
+
+  def order_trainers
+    Trainer.all.sort_by(&:updated_at)
+  end
+  #
+
+  # def check_trainer
+  #   if self.trainer_lead_interviews.length == 0 && self.company_lead_interviews.length == 0 && self.occupied == true
+  #     byebug
+  #     self.update(occupied: false)
+  #   elsif self.trainer_lead_interviews.length == 0 && self.company_lead_interviews.length == 0 && self.occupied == false
+  #     byebug
+  #     return self
+  #   elsif self.trainer_lead_interviews.length > 0 || self.company_lead_interviews.length > 0 && self.occupied == false
+  #     byebug
+  #     self.update(occupied: true)
+  #   elsif self.trainer_lead_interviews.length > 0 || self.company_lead_interviews.length > 0 && self.occupied == true
+  #     byebug
+  #     return self
+  #   end
+  # end
+  # def filter_trainers
+  #   filtered_trainers = Trainer.all.where(:occupied => false, :hold=> false)
+  #   filtered_trainers.sort_by(&:updated_at)
+  # end
+
 end
