98eb5c3e691e87cecf0ca643eb4425df38fe1ed6
Matthew Croak <matthewcroak@Matthews-MBP.home>
2017-12-19 10:14:52 -0500

    fixed mailer bu

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: 587,
     domain: "gmail.com",
-    authentication: nil,
+    authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
