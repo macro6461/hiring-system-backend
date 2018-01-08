410ec83272ccc681d159559ca0c418f6ae37965d
Matthew Croak <matthewcroak@Matthews-MBP.home>
2018-01-08 18:30:04 -0500

    finally getting around to trainer lead frontend functionality

--- app/controllers/company_lead_rsvp_tickets_controller.rb
+++ app/controllers/company_lead_rsvp_tickets_controller.rb
@@ -12,27 +12,20 @@ class CompanyLeadRsvpTicketsController < ApplicationController
     end
 
     def create
-      byebug
-      @company_lead = CompanyLead.where(:first_name=>params[:first_name], :last_name=>params[:last_name]).first
-      @company_lead_rsvp_ticket = CompanyLeadRsvpTicket.where(:otp_secret_key=>params[:otp_secret_key]).first || CompanyLeadRsvpTicket.where(:company_lead_id=>@company_lead.id).first
-      respond_to do |format|
-        if @company_lead_rsvp_ticket.save
-        byebug
-          # Tell the CompanyLeadRsvpTicketMailer to send an email after save containing the ticket
-          CompanyLeadRsvpTicketMailer.with(company_lead_rsvp_ticket: @company_lead_rsvp_ticket).company_lead_rsvp_ticket(@company_lead_rsvp_ticket).deliver_now
 
-          format.html { redirect_to(@company_lead_rsvp_ticket, notice: 'Company Lead RSVP Ticket was successfully created.') }
-          format.json { render json: @company_lead_rsvp_ticket, status: :created, location: @company_lead_rsvp_ticket }
-
-          # render json: {company_lead_rsvp_ticket: @company_lead_rsvp_ticket}
-        else
-          format.html { render action: 'new' }
-          format.json { render json: @company_lead_rsvp_ticket.errors, status: :unprocessable_entity }
-
-          render json: {error: @company_lead_rsvp_ticket.errors.messages.first}, status: 406
-        end
+      if params[:otp_secret_key].length > 0
+        @company_lead_rsvp_ticket = CompanyLeadRsvpTicket.where(:otp_secret_key=>params[:otp_secret_key]).first
+      elsif params[:first_name].length > 0 && params[:last_name].length > 0
+        @company_lead_rsvp_ticket = CompanyLeadRsvpTicket.where(:company_lead_id=>@company_lead.id).first
+      elsif params[:confirmation].length > 0
+        @company_lead_rsvp_ticket = CompanyLeadRsvpTicket.where(:confirmation=>params[:confirmation]).first
       end
-
+      
+      if @company_lead_rsvp_ticket
+        render json: {lead: @company_lead_rsvp_ticket.company_lead, ticket: @company_lead_rsvp_ticket}
+      else
+        render json: {error: "RSVP not found"}
+      end
     end
 
     def update
