7cff6b2854a2f0400f7193baddbb1ad351dca13c
Matthew Croak <matthewcroak@Matthews-MBP.home>
2018-01-12 11:25:18 -0500

    working on action mailer heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,20 +80,20 @@ config.action_mailer.delivery_method = :smtp
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
-  #mailing
-  config.action_mailer.delivery_method = :smtp
 
+  #mailing
+  config.action_mailer.default_url_options = { :host => 'integrated-hiring-backend.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "gmail.com",
+    domain: 'integrated-hiring-backend.herokuapp.com',
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
-  }
-
-
-    config.action_mailer.default_url_options = { :host => ENV["HEROKU_HOST"] }
+}
 end
