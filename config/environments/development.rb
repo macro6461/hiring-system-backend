c2b70890cbca81d5f48e3672c1b8af5d8981f0b9
Matthew Croak <matthewcroak@Matthews-MBP.home>
2018-01-12 10:39:58 -0500

    changed host for action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,5 @@ Rails.application.configure do
   }
 
 
-    config.action_mailer.default_url_options = { :host => "localhost:3000" }
+    config.action_mailer.default_url_options = { :host => ENV["HEROKU_HOST"] }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,20 @@ config.action_mailer.delivery_method = :smtp
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  #mailing
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
+
+    config.action_mailer.default_url_options = { :host => ENV["HEROKU_HOST"] }
 end
