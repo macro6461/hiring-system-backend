40533ce93546890ccc88d506b1bf07978cbebd55
Matthew Croak <matthewcroak@Matthews-MBP.home>
2018-01-12 10:06:53 -0500

    why

--- app/serializers/trainer_serializer.rb
+++ app/serializers/trainer_serializer.rb
@@ -3,11 +3,3 @@ class TrainerSerializer < ActiveModel::Serializer
 
   has_many :trainer_leads
 end
-
-t.string :first_name
-t.string :last_name
-t.string :email_address
-t.string :phone_number
-t.string :url
-t.boolean :occupied, default: false
-t.boolean :hold, default: false
