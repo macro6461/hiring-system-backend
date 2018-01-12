44aa509e98e0d94605d97cc8ada397b878f846b3
Matthew Croak <matthewcroak@Matthews-MBP.home>
2018-01-12 11:41:35 -0500

    working on mailer still

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,18 +47,18 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = { :host => 'integrated-hiring-backend.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
+
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: 'gmail.com',
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
-}
+  }
+
+
+    config.action_mailer.default_url_options = { :host => "localhost:3000" }
 end
