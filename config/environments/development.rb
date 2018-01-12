b4ea5bb646dcc75426f75ae3812a4872c7642ce7
Matthew Croak <matthewcroak@Matthews-MBP.home>
2018-01-12 11:32:49 -0500

    changing domain name

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: 'integrated-hiring-backend.herokuapp.com',
+    domain: 'gmail.com',
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: 'integrated-hiring-backend.herokuapp.com',
+    domain: 'gmail.com',
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
