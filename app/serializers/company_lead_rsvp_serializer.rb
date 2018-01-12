8b3a65a0666f9de013df28a31f786f28695fa932
Matthew Croak <matthewcroak@Matthews-MBP.home>
2018-01-12 10:05:25 -0500

    no idea

--- app/serializers/company_lead_rsvp_serializer.rb
+++ app/serializers/company_lead_rsvp_serializer.rb
@@ -2,7 +2,7 @@ class CompanyLeadRsvpSerializer < ActiveModel::Serializer
   attributes :id, :title, :start_date, :end_date, :description, :location, :company_lead_id, :checked_in, :event_id
 
 
-  belongs_to :company_lead
-  belongs_to :event
+  # belongs_to :company_lead
+  # belongs_to :event
 
 end
--- app/serializers/trainer_lead_rsvp_serializer.rb
+++ app/serializers/trainer_lead_rsvp_serializer.rb
@@ -1,7 +1,7 @@
 class TrainerLeadRsvpSerializer < ActiveModel::Serializer
   attributes :id, :title, :start_date, :end_date, :description, :location, :trainer_lead_id, :checked_in, :event_id, :trainer_id
 
-  belongs_to :event
-  belongs_to :trainer_lead
+  # belongs_to :event
+  # belongs_to :trainer_lead
 
 end
