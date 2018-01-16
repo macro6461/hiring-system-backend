f495260ee6186666c67c47ff77c956ca7f2468a7
Matthew Croak <matthewcroak@Matthews-MBP.home>
2018-01-16 13:36:32 -0500

    perfected the conditional round robin assignment. YAS

--- app/models/company_lead_interview.rb
+++ app/models/company_lead_interview.rb
@@ -2,7 +2,7 @@ class CompanyLeadInterview < ApplicationRecord
 
   # belongs_to :trainer
   belongs_to :company_lead
-  after_save :update_company_lead, :update_trainer
+  after_save :update_company_lead, :check_trainer
 
   def trainer
     if self.trainer_id == nil
@@ -12,32 +12,27 @@ class CompanyLeadInterview < ApplicationRecord
     end
   end
 
+  def check_trainer #does not change the status of a trainer if there is a reference. That trainer doesn't change if they are a reference.
+    if self.reference == true
+      self.trainer
+    elsif self.reference == false
+      self.update_trainer
+    end
+  end
+
+
   def update_trainer
     if self.trainer_id == nil
       "no trainer assigned"
     else
       is_hired = self.hire
       company_lead_interview_trainer = Trainer.all.find(self.trainer_id)
-      byebug
-      if self.reference == true && (is_hired == "N/A" || is_hired == nil)
-        byebug
-        return self.trainer.update(occupied: false)
-      else
-        if is_hired == "N/A" || is_hired == nil
-          if self.reference == true
-            company_lead_interview_trainer.update(occupied: false)
-          else
-            company_lead_interview_trainer.update(occupied: true)
-          end
-        elsif is_hired == "yes"
-          company_lead_interview_trainer.update(occupied: false)
-          self.update(trainer_id: nil)
-          # return company_lead_interview_trainer
-        elsif is_hired == "no"
-          company_lead_interview_trainer.update(occupied: false)
-          self.update(trainer_id: nil)
-          # return company_lead_interview_trainer
-        end
+      if is_hired == "N/A" || is_hired == nil
+        company_lead_interview_trainer.update(occupied: true)
+        # return company_lead_interview_trainer
+      elsif is_hired == "yes" || is_hired == "no"
+        company_lead_interview_trainer.update(occupied: false)
+        # return company_lead_interview_trainer
       end
     end
   end
